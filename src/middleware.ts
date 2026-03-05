de4a0d431e333a95b96089ecb1a4c1986d24d4a0
ctrl-h1101241002 <h1101241002@student.untan.ac.id>
2026-03-05 14:00:00 +0700

    PRAKTIKUM 5 - beforeHandle

---
+++ src/middleware.ts
@@ -0,0 +1,59 @@
+import { Elysia } from "elysia"
+import { openapi } from "@elysiajs/openapi";
+
+
+const app = new Elysia()
+.use(openapi())
+// Global Logger
+app.onRequest(({ request }) => {
+ console.log("📥", request.method, request.url)
+ console.log("🕒", new Date().toISOString())
+})
+
+app.onRequest(({ request, set }) => {
+  if (request.headers.get("x-block") === "true") {
+    set.status = 403
+    return { message: "Blocked" }
+  }
+})
+
+app.get("/", () => "Hello Middleware")
+
+app.get(
+ "/dashboard",
+ () => ({
+   message: "Welcome to Dashboard"
+ }),
+ {
+   beforeHandle({ headers, set }) {
+     if (!headers.authorization) {
+       set.status = 401
+       return {
+         success: false,
+         message: "Unauthorized"
+       }
+     }
+   }
+ }
+)
+
+app.get(
+  "/admin",
+   () => ({
+   stats:99
+ }),
+ {
+    beforeHandle({ headers, set }) {
+      if (headers.authorization !== "Bearer 123") {
+        set.status = 401;
+        return {
+          success: false,
+          message: "Unauthorized"
+        };
+      }
+    }
+  }
+);
+
+app.listen(3000)
+console.log("Server running at http://localhost:3000")
